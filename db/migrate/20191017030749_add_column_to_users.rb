cfa36839d18f58643ac24f0935e522eee3186f2d
miyamo93 <miyamo0802@gmail.com>
2019-10-28 14:35:59 +0900

    Update 20191017030749_add_column_to_users.rb

--- db/migrate/20191017030749_add_column_to_users.rb
+++ db/migrate/20191017030749_add_column_to_users.rb
@@ -1,5 +1,5 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :name, :string , null: false, unique: true, index: tru
+    add_column :users, :name, :string , null: false, unique: true, index: true
   end
 end
